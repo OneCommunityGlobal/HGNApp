7146ccee65767b50c45465c9d49a234ac44779b5
Shubhra Mittal <shubhra.goel@gmail.com>
2018-02-28 11:04:37 -0800

    Changed user profile form and moved it to a component

--- app/components/hgn-editor.js
+++ app/components/hgn-editor.js
@@ -1,29 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didRender()
-  {
+  didRender() {
+
     tinymce.init({
-  selector: 'textarea',
-  height: 300,
-  theme: 'modern',
-  plugins: [
-    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars code fullscreen',
-    'insertdatetime media nonbreaking save table contextmenu directionality',
-    'emoticons template paste textcolor colorpicker textpattern imagetools'
-  ],
-  toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  toolbar2: 'print preview media | forecolor backcolor emoticons',
-  image_advtab: true,
-  templates: [
-    { title: 'Test template 1', content: 'Test 1' },
-    { title: 'Test template 2', content: 'Test 2' }
-  ],
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
- });
+      selector: 'textarea',
+      height: 200,
+      menubar: false,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor textcolor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code help wordcount'
+      ],
+      toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+      content_css: [
+        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+        '//www.tinymce.com/css/codepen.min.css']
+    });
   }
 });
---
+++ app/components/user-profile.js
@@ -0,0 +1,146 @@
+import Ember from 'ember';
+import UserProfileValidationMixin from '../mixins/user-profile-validation-mixin';
+
+export default Ember.Component.extend(UserProfileValidationMixin, {
+    self: this,
+    userProfileService: Ember.inject.service('user-profile-service'),
+    showErrors: true,
+    showFormErrors: false,
+    newPersonalLink: {
+        Name: "",
+        Link: ""
+    },
+    newAdminLink: {
+        Name: "",
+        Link: ""
+    },
+    newProfilePic: "",
+    newLinkformValidate: false,
+
+    isLoggedinUserAdministrator: Ember.computed('userrole', function () {
+
+        let userrole = this.get('userrole');
+        return userrole === "Administrator" ? true : false;
+    
+      }),
+    
+      canUserEditPersonalFields: Ember.computed('model', 'userId', 'userrole', function () {
+    
+        let loggedinUser = this.get('userId');
+        let viewingProfileOf = this.get('model._id');
+        let userrole = this.get('userrole');
+    
+        return (loggedinUser === viewingProfileOf || userrole === "Administrator") ? true : false;
+    
+    
+      }),
+
+    actions: {
+
+        updateProfilePic: function (event) {
+            const reader = new FileReader();
+            const file = event.target.files[0];
+            let imageData;
+
+            reader.onload = () => {
+                imageData = reader.result;
+                this.set('model.profilePic', imageData);
+
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        },
+
+
+        postChanges() {
+            let userId = this.get('model._id');
+            let user = this.get('model');
+
+
+
+            this.validate()
+                .then(() => {
+
+                    this.get('userProfileService').editUserProfileData(user, userId)
+                })
+                .then(results => {
+
+                    toastr.success("", 'Changes Saved');
+                }, error => {
+                    console.log(error);
+                    toastr.warning(error.responseJSON.message, 'Error!!');
+                });
+
+
+
+        },
+        removeTeamMembership(team) {
+            var result = confirm("Are you sure you want to delete membership to this team?");
+            if (result) {
+                this.get('model.teamId').removeObject(team);
+            }
+        },
+
+        addPersonalLink() {
+
+            this.set('newLinkformValidate', true);
+
+            this.validate()
+                .then(() => {
+                    this.get('model.personalLinks').addObject(this.get('newPersonalLink'));
+                    this.set('newPersonalLink', {});
+                    this.set('newLinkformValidate', false);
+                })
+                .catch(() => {
+                    this.set('showFormErrors', true);
+                })
+
+                ;
+
+        },
+
+        CancelAddingPersonalLink() {
+
+            this.set('newLinkformValidate', false);
+            this.set('newPersonalLink', {});
+
+        },
+
+        CancelAddingAdminLink() {
+
+            this.set('newLinkformValidate', false);
+            this.set('newAdminLink', {});
+        },
+
+        removePersonalLink(personallink) {
+            var result = confirm("Are you sure you want to remove this link?")
+            if (result) {
+                this.get('model.personalLinks').removeObject(personallink);
+            }
+        },
+        addAdminLink() {
+
+            let newLink = this.get('newAdminLink');
+
+            if (newLink.Name.length < 1 || newLink.Link.length < 1 || !this.isLoggedinUserAdministrator) {
+
+            } else {
+                this.get('model.adminLinks').addObject(this.get('newAdminLink'));
+                this.set('newAdminLink', {});
+            }
+
+
+        },
+        removeAdminLink(adminLink) {
+            var result = confirm("Are you sure you want to delete this link?");
+            if (result) {
+                if (this.isLoggedinUserAdministrator) {
+                    this.get('model.adminLinks').removeObject(adminLink);
+                }
+            }
+        }
+
+    }
+});
