818449914bdf4d816465455256c7ac726b274dcc
Shubhra Mittal <shubhra.goel@gmail.com>
2018-03-13 13:01:16 -0700

    updates to dashboard

--- app/components/monthly-effort-chart.js
+++ app/components/monthly-effort-chart.js
@@ -13,67 +13,26 @@ export default Component.extend({
             .then(() => {
 
                 let monthlydata = this.get('laborthismonth');
-                let labels = [];
+                let maxeffort = 0;
                 let data = [];
-                let backgroundColor = [];
 
                 monthlydata.forEach(element => {
+                    let effort = element.timeSpent_hrs;
+                    maxeffort = (maxeffort > effort) ? maxeffort : effort;
 
-                    labels.push(element.projectName);
-                    data.push(parseFloat(element.timeSpent_hrs).toFixed(2));
-
-                    // backgroundColor.push(`#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`)
-
-                    backgroundColor.push("white")
-
+                    data.push(
+                        {
+                            projectName: element.projectName,
+                            effort: parseFloat(element.timeSpent_hrs).toFixed(2)
+                        }
+                    );
 
                 });
+                this.set("maxeffort", maxeffort);
+                this.set("resultset", data)
 
-                let results = {
-                    labels: labels,
-                    datasets: [{
-                        label: "Effort Spent",
-                        data: data,
-                        borderWidth: 1,
-                        backgroundColor: backgroundColor
-                    }]
-                }
 
-                this.set('hoursthismonth', results)
 
             })
-    },
-
-    baroptions:
-        {
-            layout: {
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                }
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }],
-                xAxes: [{
-                    ticks: {
-                        autoSkip: false
-                    }
-                }]
-            },
-            legend: {
-                display: true,
-                usePointStyle: true,
-                position: "bottom",
-                labels: {
-
-                }
-            }
-
-        },
+    }
 });
