891a95fad5cd33fb9955e0fd1bf4445b7e75f64e
Shubhra Mittal <shubhra.goel@gmail.com>
2018-03-19 17:36:40 -0700

    changes to header

--- app/components/user-profile.js
+++ app/components/user-profile.js
@@ -25,6 +25,8 @@ export default Component.extend({
         let userrole = this.get('userrole');
         return userrole === "Administrator" ? true : false;
 
+
+
     }),
 
     canUserEditPersonalFields: computed('model', 'userId', 'userrole', function () {
