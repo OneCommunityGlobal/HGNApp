bae730d16f56f71b114ceef8623b6e0a8a82e42b
asha <ashaagm@gmail.com>
2018-03-29 15:26:50 -0400

    some badge changes

--- app/routes/badges.js
+++ app/routes/badges.js
@@ -1,5 +1,16 @@
 
-import Route from '@ember/routing/route';
+// import Route from '@ember/routing/route';
 
-export default Route.extend({
+// export default Route.extend({
+// });
+
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  queryParams: {
+    category: {
+      replace: true
+    }
+  }
 });
