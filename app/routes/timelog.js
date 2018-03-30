c3d6f1d170720af68dee4ddba39f99d65662a6ad
Shubhra Mittal <shubhra.goel@gmail.com>
2018-03-29 17:27:49 -0700

    some  alert to check the page status

--- app/routes/timelog.js
+++ app/routes/timelog.js
@@ -9,6 +9,7 @@ export default Route.extend(UnAuthenticatedRouteMixin, {
 
 
     let forUserId = params.user_id;
+
     return this.get('userProfileService').getUserName(forUserId)
       .then((results) => {
 
@@ -16,7 +17,7 @@ export default Route.extend(UnAuthenticatedRouteMixin, {
           "name": results.name,
           "forUserId": forUserId
         }
-
+        alert("loading");
         return result;
 
       })
