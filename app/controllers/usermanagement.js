75f49139b0f9c252c49ff44cbe1d401af25b0b18
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-04-19 19:35:54 -0400

    Added filter for active and inactive users

--- app/controllers/usermanagement.js
+++ app/controllers/usermanagement.js
@@ -1,17 +1,43 @@
 
+import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 import Controller from '@ember/controller';
 export default Controller.extend({
     
     userProfileService: inject('user-profile-service'),
+    users: alias('model'),
+    currentFilter: null,
+
+    filteredUsers: computed('users.@each.isActive', 'currentFilter', function() {
+      if (this.get('currentFilter') === null) {
+        return this.get('users');
+      } else {
+        var isTrueSet = (this.get('currentFilter') == 'true');
+        return this.get('users').filter(user => user.isActive === isTrueSet);
+      }
+    }),
+    // filteredPeople: filter('users', function(user) {
+    //   if(this.get('currentFilter') === null) {
+    //     return true;
+    //   } else {
+       
+    //     return user.isActive === true;
+    //   }
+    // }).property('users', 'currentFilter'),
 
     actions: {
        
-
-        activeFilter: function () {
-         return this.get('userProfileService').getActiveUserProfiles();
+      filterUpdated: function (value) {
+        
+        alert(value);
+        if (value == "null") {
+          this.set('currentFilter', null);
         }
-
+        else {
+          this.set('currentFilter', value);
+        }
+      }
     },
     
     columns:[
@@ -33,16 +59,17 @@ export default Controller.extend({
         {
             "propertyName": "role",
             "title":"Role",
-            "filterWithSelect":true
+            "filterWithSelect":true,
+            "disableSorting":true
           },
           {
             "propertyName": "email",
             "title":"Email",
-            "sorting":false
+            "disableSorting":true
           },
           {
             "propertyName": "weeklyComittedHours",
-            "title":"Weekly Committed hours"
+            "title":"Weekly Committed hrs"
           }
         ],
 
