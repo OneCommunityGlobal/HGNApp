d7093903140cfd5cc1566d0c0161cf4b255b43f3
Shubhra Mittal <shubhra.goel@gmail.com>
2018-04-26 14:14:40 -0700

    instantiated field

--- app/components/user-profile.js
+++ app/components/user-profile.js
@@ -17,6 +17,7 @@ export default Component.extend({
     showErrors: true,
     showFormErrors: false,
     newProfilePic: "",
+    showwarning: "",
 
     options: {
         plugins: ["link", "autolink"],
