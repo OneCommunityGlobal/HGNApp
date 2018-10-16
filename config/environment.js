fcbe7f4b90701c3b955671ac84351f5e950a8018
Shubhra Mittal <shubhra.goel@gmail.com>
2018-10-15 18:24:47 -0700

    checking builds

--- config/environment.js
+++ config/environment.js
@@ -14,6 +14,7 @@ module.exports = function (environment) {
       'connect-src': "'self' https://hgn-rest.azurewebsites.net/api"
     },
     defaultPwd:"123Welcome!",
+    BRANCH_NAME : process.env.CIRCLE_BRANCH,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -24,6 +25,7 @@ module.exports = function (environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+     
 
     }
   };
